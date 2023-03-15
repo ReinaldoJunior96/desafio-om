a618da9425b6f5616b461110fa01350c1643c37e
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2023-03-15 19:39:43 -0300

    feat: adicionando Endereco factory nas seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Endereco;
+use App\Models\Paciente;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Paciente::factory()->count(5)->create();
+        Endereco::factory()->count(5)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
