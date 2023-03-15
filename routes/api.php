6e54a6541f9a1ef359c1c7c94545216f560fa9a3
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2023-03-15 20:09:48 -0300

    feat: Adicionando rotas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PacienteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('pacientes')->group(function(){
+   Route::controller(PacienteController::class)->group(function(){
+       Route::get('all','index');
+   }) ;
 });
