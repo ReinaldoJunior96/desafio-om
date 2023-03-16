2ac21ad6c17982049b48fed1e9198234ee9fdbad
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2023-03-15 22:50:03 -0300

    feat: método para tratamento de imagem | fix: corrigindo campo no banco de dados

--- app/Models/Paciente.php
+++ app/Models/Paciente.php
@@ -5,13 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Support\Facades\Storage;
 
 class Paciente extends Model
 {
     use HasFactory;
 
     protected $table = 'pacientes';
-    protected $fillable = ['foto', 'nameCompleto', 'nomeMae', 'datanascimento', 'cpf', 'cns'];
+    protected $fillable = ['foto', 'nomeCompleto', 'nomeMae', 'datanascimento', 'cpf', 'cns'];
+
+    public function uploadFoto($foto)
+    {
+        if (Storage::exists($foto)) {
+            Storage::delete($foto);
+        }
+
+        $upload = Storage::put('public/pacientes/images', $foto);
+
+        if (!$upload) {
+            return response()->json('Não foi possível realizar o upload da imagem!', 409);
+        }
+
+        return str_replace('public', '/storage', $upload);
+
+
+
+    }
+
 
     public function endereco(): HasOne
     {
