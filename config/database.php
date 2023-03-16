a24aed5156a7c8fccbbecc040cdbf46ca3dbd91b
Reinaldo Júnior <33222779+ReinaldoJunior96@users.noreply.github.com>
2023-03-16 11:35:15 -0300

    feat: instalando e configurando redis

--- config/database.php
+++ config/database.php
@@ -121,11 +121,12 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'parameters' => ['password' => env('REDIS_PASSWORD', null)],
         ],
 
         'default' => [
@@ -145,6 +146,7 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
+
 
     ],
 
